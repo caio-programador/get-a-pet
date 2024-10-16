66088f000e42b6e3816b243ae6a0712568a6e098
Caio Flôres <caio.florees2005@gmail.com>
2024-10-16 15:31:27 -0300

    Creating register Routes and Controller for User

---
+++ controllers/UserController.js
@@ -0,0 +1,42 @@
+const User = require("../models/User")
+
+module.exports = class UserController{
+    static async register(req, res){
+        const {name, email, phone, password, confirmpassword} = req.body
+
+        if(!name) {
+            res.status(422).json({message: 'O nome é obrigatório'})
+            return
+        }
+        if(!email) {
+            res.status(422).json({message: 'O e-mail é obrigatório'})
+            return
+        }
+        if(!phone) {
+            res.status(422).json({message: 'O número de telefone é obrigatório'})
+            return
+        }
+        if(!password) {
+            res.status(422).json({message: 'A senha é obrigatória'})
+            return
+        }
+        if(!confirmpassword) {
+            res.status(422).json({message: 'A confirmação de senha é obrigatória'})
+            return
+        }
+
+        if(password !== confirmpassword){
+            res.status(422).json({message: 'As senhas não coincidem'})
+            return
+        }
+
+        const userExists = await User.findOne({email:email})
+
+        if(userExists){
+            res.status(422).json({message: 'Por favor, utilize outro e-mail'})
+            return
+        }
+        
+
+    }
+}
